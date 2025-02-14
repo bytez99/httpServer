5c48525ca98cb459edd8b2b2a1ce30ca581080f4
derice <bytez9001@gmail.com>
2025-02-14 21:49:23 +0000

    Added 404 Not found response, also added while loop to keep checking for new connections

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,26 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
 
 public class Main {
   public static void main(String[] args) {
+    while(true) {
+        try {
 
-     try {
-       ServerSocket serverSocket = new ServerSocket(4221);
-       serverSocket.setReuseAddress(true);
+            ServerSocket serverSocket = new ServerSocket(4221);
+            serverSocket.setReuseAddress(true);
 
-       HandleClient(serverSocket.accept());
+            Socket socket = serverSocket.accept();
+            HandleClient(socket);
+            serverSocket.close();
 
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
 
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+            System.out.println("Server Socket");
+        }
+    }
 
   }
 
@@ -22,12 +28,32 @@ public class Main {
       System.out.println("Accepted new connection");
       try {
 
-          socket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+          InputStream inputStream = socket.getInputStream();
+          BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+          OutputStream outputStream = socket.getOutputStream();
+
+          String requestLine = reader.readLine();
+          String[] requestLines = requestLine.split(" ");
+
+//          for (int i = 0; i < requestLines.length; i++) {
+//              System.out.println(requestLines[i]);
+//          }
+
+          if (Objects.equals(requestLines[1],"/")){
+              outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+          }else {
+              outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+          }
+
+
           socket.close();
+
 
 
       }catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
+          System.out.println("Client Handling");
       }
   }
 
