eb2b7da5fbdf9eaa30011b22b8d4dab1c385bc52
derice <bytez9001@gmail.com>
2025-02-15 20:15:35 +0000

    Made HTTP Server use concurrency to handle multiple connections at once.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,88 +1,98 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Main {
-  public static void main(String[] args) {
-    while(true) {
-        try {
 
-            ServerSocket serverSocket = new ServerSocket(4221);
-            serverSocket.setReuseAddress(true);
+public class Main   {
+    private static final int PORT = 4221;
 
+  public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        ExecutorService executor = Executors.newCachedThreadPool();
+        serverSocket.setReuseAddress(true);
+        while (true) {
             Socket socket = serverSocket.accept();
-            HandleClient(socket);
-            serverSocket.close();
-
-
-        } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-            System.out.println("Server Socket");
+            executor.execute(new HandleConnection(socket));
+            }
         }
     }
 
-  }
+class HandleConnection implements Runnable{
+    private Socket socket;
+    public HandleConnection(Socket socket) {
+        this.socket = socket;
+    }
+    public void run(){
+            try {
+                HandleClient(socket);
+                socket.close();
 
-  public static void HandleClient(Socket socket) {
-      System.out.println("Accepted new connection");
-      try {
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+    }
 
-          InputStream inputStream = socket.getInputStream();
-          BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+    public static void HandleClient(Socket socket) {
+        System.out.println("Accepted new connection");
+        try {
 
-          OutputStream outputStream = socket.getOutputStream();
+            InputStream inputStream = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-          String userAgent = null;
-          String host = null;
-          String requestLine = reader.readLine();
+            OutputStream outputStream = socket.getOutputStream();
 
-          String line;
-          while ((line = reader.readLine()) != null && !line.isEmpty()) {
-              String trim = line.substring(line.indexOf(":") + 1).trim();
-              if(line.toLowerCase().startsWith("user-agent:")){
-                  userAgent = trim;
-              } else if (line.toLowerCase().startsWith("host:")) {
-                  host = trim;
-              }
-          }
+            String userAgent = null;
+            String host = null;
+            String requestLine = reader.readLine();
 
-          System.out.println("HTTP Method: " + requestLine);
-          System.out.println("Host: " + host);
-          System.out.println("User-Agent: " + userAgent);
+            String line;
+            while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                String trim = line.substring(line.indexOf(":") + 1).trim();
+                if(line.toLowerCase().startsWith("user-agent:")){
+                    userAgent = trim;
+                } else if (line.toLowerCase().startsWith("host:")) {
+                    host = trim;
+                }
+            }
 
-
-
-          String[] requestLines = requestLine.split(" ");
-
-          String httpMethod = requestLines[0];
-          String requestTarget = requestLines[1];
-          String httpVersion = requestLines[2];
+            System.out.println("HTTP Method: " + requestLine);
+            System.out.println("Host: " + host);
+            System.out.println("User-Agent: " + userAgent);
 
 
-          if (requestTarget.equals("/")){
-              outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-          } else if (requestTarget.contains("/echo") && requestTarget.length() > 5) {
-              String echo = requestTarget.substring(requestTarget.lastIndexOf("/") + 1, requestTarget.length());
-              System.out.println("echo: " + echo);
-              String echoString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=UTF-8\r\nContent-Length: 3\r\n\r\n" + echo;
-              outputStream.write(echoString.getBytes());
-          } else if(requestTarget.equals("/user-agent")){
+
+            String[] requestLines = requestLine.split(" ");
+
+            //String httpMethod = requestLines[0];
+            String requestTarget = requestLines[1];
+            //String httpVersion = requestLines[2];
+
+
+            if (requestTarget.equals("/")){
+                outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            } else if (requestTarget.contains("/echo") && requestTarget.length() > 5) {
+                String echo = requestTarget.substring(requestTarget.lastIndexOf("/") + 1, requestTarget.length());
+                System.out.println("echo: " + echo);
+                String echoString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=UTF-8\r\nContent-Length: 3\r\n\r\n" + echo;
+                outputStream.write(echoString.getBytes());
+            } else if(requestTarget.equals("/user-agent")){
                 String userAgentString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=UTF-8\r\nContent-Length: 12\r\n\r\n" + userAgent;
                 outputStream.write(userAgentString.getBytes());
-          }
-          else {
-              outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-          }
+            }
+            else {
+                outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+            }
 
-          outputStream.close();
-          socket.close();
+            outputStream.close();
+            socket.close();
 
 
 
-      }catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-          System.out.println("Client Handling");
-      }
-  }
-
+        }catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+            System.out.println("Client Handling");
+        }
+    }
 }
