2ddbdd5a0d38b7264d07ddd8043a13c57d4ffd77
derice <bytez9001@gmail.com>
2025-02-15 18:54:34 +0000

    Implemented way to read user-agent and return it to the user when requestTarget = /user-agent

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLOutput;
 import java.util.Objects;
 
 public class Main {
@@ -33,20 +34,49 @@ public class Main {
 
           OutputStream outputStream = socket.getOutputStream();
 
+          String userAgent = null;
+          String host = null;
           String requestLine = reader.readLine();
+
+          String line;
+          while ((line = reader.readLine()) != null && !line.isEmpty()) {
+              String trim = line.substring(line.indexOf(":") + 1).trim();
+              if(line.toLowerCase().startsWith("user-agent:")){
+                  userAgent = trim;
+              } else if (line.toLowerCase().startsWith("host:")) {
+                  host = trim;
+              }
+          }
+
+          System.out.println("HTTP Method: " + requestLine);
+          System.out.println("Host: " + host);
+          System.out.println("User-Agent: " + userAgent);
+
+
+
           String[] requestLines = requestLine.split(" ");
 
-//          for (int i = 0; i < requestLines.length; i++) {
-//              System.out.println(requestLines[i]);
-//          }
+          String httpMethod = requestLines[0];
+          String requestTarget = requestLines[1];
+          String httpVersion = requestLines[2];
 
-          if (Objects.equals(requestLines[1],"/")){
+
+          if (requestTarget.equals("/")){
               outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-          }else {
+          } else if (requestTarget.contains("/echo") && requestTarget.length() > 5) {
+              String echo = requestTarget.substring(requestTarget.lastIndexOf("/") + 1, requestTarget.length());
+              System.out.println("echo: " + echo);
+              String echoString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=UTF-8\r\nContent-Length: 3\r\n\r\n" + echo;
+              outputStream.write(echoString.getBytes());
+          } else if(requestTarget.equals("/user-agent")){
+                String userAgentString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=UTF-8\r\nContent-Length: 12\r\n\r\n" + userAgent;
+                outputStream.write(userAgentString.getBytes());
+          }
+          else {
               outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
           }
 
-
+          outputStream.close();
           socket.close();
 
 
